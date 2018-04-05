e1e93c0316859ea2ca57be8df5a32e7ef6ce31c4
naomichi-y <n.yamakita@gmail.com>
2018-04-05 23:31:20 +0900

    Refactor docker-compose

---
+++ lib/genova/deploy/mutex.rb
@@ -0,0 +1,27 @@
+module Genova
+  module Deploy
+    class Mutex
+      def initialize(key, ttl = 1800)
+        @key = key
+        @ttl = ttl
+      end
+
+      def lock
+        $redis.multi do
+          return false unless $redis.setnx(@key, true)
+          $redis.expire(@key, @ttl)
+        end
+
+        true
+      end
+
+      def locked?
+        $redis.get(@key) || false
+      end
+
+      def unlock
+        $redis.del(@key) == 1
+      end
+    end
+  end
+end
