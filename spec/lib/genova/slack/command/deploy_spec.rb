a0a37450b2aa6a0d888429ae260ed0b44fdd983a
naomichi-y <n.yamakita@gmail.com>
2018-04-07 01:51:22 +0900

    Add rspec

--- spec/lib/genova/slack/command/deploy_spec.rb
+++ spec/lib/genova/slack/command/deploy_spec.rb
@@ -3,11 +3,31 @@ require 'rails_helper'
 module Genova
   module Slack
     module Command
-      # describe Deploy do
-      #   it 'should be return help message' do
-      #     expect(message: "#{SlackRubyBot.config.user} deploy", channel: 'channel').to respond_with_slack_message(/I am ECS deploy Bot\./)
-      #   end
-      # end
+      describe Deploy do
+        let(:bot_mock) { double(Genova::Slack::Bot) }
+
+        context 'when manual deploy' do
+          it 'should be return confirm message' do
+            allow(bot_mock).to receive(:post_confirm_deploy)
+            allow(Genova::Slack::Bot).to receive(:new).and_return(bot_mock)
+
+            command = "#{SlackRubyBot.config.user} deploy repository master cluster:service"
+            expect(message: command, channel: 'channel').to not_respond
+            expect(bot_mock).to have_received(:post_confirm_deploy).once
+          end
+        end
+
+        context 'when interactive deploy' do
+          it 'should be return repositories' do
+            allow(bot_mock).to receive(:post_choose_repository)
+            allow(Genova::Slack::Bot).to receive(:new).and_return(bot_mock)
+
+            command = "#{SlackRubyBot.config.user} deploy"
+            expect(message: command, channel: 'channel').to not_respond
+            expect(bot_mock).to have_received(:post_choose_repository).once
+          end
+        end
+      end
     end
   end
 end
