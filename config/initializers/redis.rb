046438849e15d2395ba850e031438732b6716b27
naomichi-y <n.yamakita@gmail.com>
2018-04-13 17:49:48 +0900

    Refactor

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 require 'erb'
 
-REDIS_CONFIG = YAML.load(ERB.new(IO.read(Rails.root.join('config/redis.yml'))).result).symbolize_keys
-config = REDIS_CONFIG[:default].symbolize_keys
-config = config.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
+config = YAML.load(ERB.new(IO.read(Rails.root.join('config/redis.yml'))).result).deep_symbolize_keys
 
-Redis.current = Redis.new(config)
+Redis.current = Redis.new(config[Rails.env.to_sym])
 Redis.current.ping
