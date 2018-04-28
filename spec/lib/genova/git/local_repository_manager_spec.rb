4bd9fd9c46fd0047ea8274e0b3725a7937b93527
naomichi-y <n.yamakita@gmail.com>
2018-04-29 03:51:22 +0900

    Add rspec

--- spec/lib/genova/git/local_repository_manager_spec.rb
+++ spec/lib/genova/git/local_repository_manager_spec.rb
@@ -59,6 +59,26 @@ module Genova
           expect(manager.open_task_definition_config('service')).to be_a(Genova::Config::TaskDefinitionConfig)
         end
       end
+
+      describe 'origin_branches' do
+        it 'should be return origin branches' do
+          branch_mock1 = double(::Git::Branch)
+          allow(branch_mock1).to receive(:name).and_return('master')
+
+          branch_mock2 = double(::Git::Branch)
+          allow(branch_mock2).to receive(:name).and_return('->')
+
+          branches_mock = double(::Git::Branches)
+          allow(branches_mock).to receive(:remote).and_return([branch_mock1, branch_mock2])
+
+          git_mock = double(::Git)
+          allow(git_mock).to receive(:fetch)
+          allow(git_mock).to receive(:branches).and_return(branches_mock)
+          allow(::Git).to receive(:open).and_return(git_mock)
+
+          expect(manager.origin_branches.size).to eq(1)
+        end
+      end
     end
   end
 end
