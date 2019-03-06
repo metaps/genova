97fef5fb8b7cb9625e14bfed656995ed1f25e204
naomichi-y <n.yamakita@gmail.com>
2019-03-06 23:58:54 +0900

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'logs#index'
-
   mount Sidekiq::Web, at: '/sidekiq'
-  mount API::Route => '/'
+  mount API::Route => '/api'
 
-  resources :logs, only: %i[index show]
-
+  root to: 'deploy_jobs#index'
+  resources :deploy_jobs, only: %i[index show]
   get '*path', controller: 'application', action: 'render_404'
 end
