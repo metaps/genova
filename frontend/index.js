295374ea2dcb66277bb54f374b8d91b0904cf1ca
naomichi-y <n.yamakita@gmail.com>
2019-04-06 03:09:02 +0900

    [#34] Setup webpack

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckCircle, faSpinner, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faCheckCircle, faSpinner, faExclamationTriangle);
+library.add(faGithub);
+dom.watch();
+
+import './assets/mystyles.scss';
