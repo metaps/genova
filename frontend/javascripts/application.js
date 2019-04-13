51ad0639c3e553e101d16ae4f404ebc870801fc5
naomichi-y <n.yamakita@gmail.com>
2019-04-14 05:21:09 +0900

    [#34] Setup webpack

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -1,3 +1,12 @@
+// import Vue from 'vue'
+// import App from './components/App'
+
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<app/>',
+// })
+
 import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
 import { faCheckCircle, faSpinner, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,34 +1,51 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: './javascripts/application.js',
   output: {
-    path: path.resolve(__dirname, '../public/assets'),
+    path: path.resolve(__dirname, 'public/assets'),
     filename: 'javascripts/application.js'
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    host: '0.0.0.0',
+    publicPath: '/assets',
+    watchContentBase: true
+  },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              plugins: [
-                require('cssnano')({ preset: 'default' })
-              ]
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('cssnano')({ preset: 'default' })
+                ]
+              },
             },
-          },
-          'sass-loader'
-        ]
-      })
-    }]
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue']
   },
   plugins: [
+    new VueLoaderPlugin(),
     new ExtractTextPlugin('stylesheets/application.css')
   ]
 };
