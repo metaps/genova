0a04c60235a55b87900076afe4bc9d7be2f501cd
EC2 Default User <n.yamakita@gmail.com>
2019-08-30 13:33:23 +0000

    [#58] Fix docker cleanup

--- lib/genova/command/docker_cleanup.rb
+++ lib/genova/command/docker_cleanup.rb
@@ -4,48 +4,67 @@ module Genova
       class << self
         def exec
           @logger = ::Logger.new(STDOUT)
-          @executor = Genova::Command::Executor.new(logger: @logger)
 
-          cleanup_stopped_images
-          cleanup_dangling_images
-          cleanup_old_images
+          if Settings.docker.cleanup
+             @logger.info('Start cleanup')
+
+             cleanup_unused_containers
+             cleanup_unused_images
+             cleanup_unused_networks
+             cleanup_unused_volumes
+          else
+            @logger.info('Cleanup is disabled')
+          end
         end
 
-        def cleanup_stopped_images
-          @logger.info('Cleanup stopped container.')
+        def cleanup_unused_containers
+          @logger.info('Cleanup unused containers')
 
-          result = @executor.command('docker container prune -f')
-          @logger.info(result)
+          ::Docker::Container.all(all: true).each do |container|
+            if container.info["State"] == 'exited'
+              @logger.info("  #{container.id}")
+              container.remove
+            end
+          end
         end
 
-        def cleanup_dangling_images
-          @logger.info('Cleanup dangling images.')
+        def cleanup_unused_images
+          @logger.info('Cleanup unused images')
+          used_images = []
 
-          result = @executor.command('docker image prune -f')
-          @logger.info(result)
-        end
-
-        def cleanup_old_images
-          @logger.info('Cleanup old images.')
-
-          result = @executor.command('docker images --format "{{.ID}}|{{.Repository}}|{{.CreatedAt}}"')
-
-          current_time = Time.new.utc.to_i
-          image_cleanup_interval = Settings.docker.image_cleanup_interval * 60 * 60 * 24
-          match_key = "#{Aws::STS::Client.new.get_caller_identity[:account]}.dkr.ecr.#{ENV.fetch('AWS_REGION')}.amazonaws.com"
-          deleted_images = 0
-
-          result.split("\n").each do |row|
-            columns = row.split('|')
-
-            next unless current_time - Time.parse(columns[2]).to_i > image_cleanup_interval && columns[1].match(/#{match_key}/)
-
-            result = @executor.command("docker rmi -f #{columns[0]}")
-            deleted_images += 1
-            @logger.info(result)
+          ::Docker::Container.all.each do |container|
+            used_images << container.info['ImageID']
           end
 
-          @logger.info("Deleted #{deleted_images} images.")
+          used_images.uniq!
+          current_time = Time.new.utc.to_i
+          image_cleanup_interval = Settings.docker.image_cleanup_interval * 60 * 60 * 24
+          ecr_image_key = "#{Aws::STS::Client.new.get_caller_identity[:account]}.dkr.ecr.#{ENV.fetch('AWS_REGION')}.amazonaws.com"
+
+          ::Docker::Image.all.each do|image|
+            remove_target = if image.info['RepoTags'][0] === '<none>:<none>'
+              true
+            elsif current_time - image.info['Created'] > image_cleanup_interval && image.info['RepoTags'][0].include?(ecr_image_key)
+              true
+            else
+              false
+            end
+
+            if remove_target && !used_images.include?(image.info['id'])
+              @logger.info("  #{image.id}")
+              image.remove(name: image.info['RepoTags'][0], force: true)
+            end
+          end
+        end
+
+        def cleanup_unused_networks
+          @logger.info('Cleanup unused networks')
+          ::Docker::Network.prune
+        end
+
+        def cleanup_unused_volumes
+          @logger.info('Cleanup unused volumes')
+          ::Docker::Volume.prune
         end
       end
     end
