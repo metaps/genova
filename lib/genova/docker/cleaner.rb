853b21a6650960c51342e57e3b6c38848089fbbe
Developer <developers@pring.jp>
2019-08-30 17:09:19 +0000

    [#58] Fix docker cleanup

--- lib/genova/docker/cleaner.rb
+++ lib/genova/docker/cleaner.rb
@@ -4,24 +4,21 @@ module Genova
       class << self
         def exec
           @logger = ::Logger.new(STDOUT)
+          @logger.info('Start cleanup')
 
-          if Settings.docker.cleanup
-             @logger.info('Start cleanup')
-
-             cleanup_unused_containers
-             cleanup_unused_images
-             cleanup_unused_networks
-             cleanup_unused_volumes
-          else
-            @logger.info('Cleanup is disabled')
-          end
+          cleanup_unused_containers
+          cleanup_unused_images
+          cleanup_unused_networks
+          cleanup_unused_volumes
         end
+
+        private
 
         def cleanup_unused_containers
           @logger.info('Cleanup unused containers')
 
           ::Docker::Container.all(all: true).each do |container|
-            if container.info["State"] == 'exited'
+            if container.info['State'] == 'exited'
               @logger.info("  #{container.id}")
               container.remove
             end
@@ -32,27 +29,26 @@ module Genova
           @logger.info('Cleanup unused images')
           used_images = []
 
-          ::Docker::Container.all.each do |container|
+          ::Docker::Container.all(all: true).each do |container|
             used_images << container.info['ImageID']
           end
 
           used_images.uniq!
           current_time = Time.new.utc.to_i
-          image_cleanup_interval = Settings.docker.image_cleanup_interval * 60 * 60 * 24
+          retention_sec = Settings.docker.retention_days * 60 * 60 * 24
           ecr_image_key = "#{Aws::STS::Client.new.get_caller_identity[:account]}.dkr.ecr.#{ENV.fetch('AWS_REGION')}.amazonaws.com"
 
           ::Docker::Image.all.each do|image|
-            remove_target = if image.info['RepoTags'][0] === '<none>:<none>'
-              true
-            elsif current_time - image.info['Created'] > image_cleanup_interval && image.info['RepoTags'][0].include?(ecr_image_key)
-              true
-            else
-              false
-            end
+            next if current_time - image.info['Created'] <= retention_sec
 
-            if remove_target && !used_images.include?(image.info['id'])
-              @logger.info("  #{image.id}")
-              image.remove(name: image.info['RepoTags'][0], force: true)
+            image.info['RepoTags'].each do |repo_tag|
+              values = repo_tag.split(':')
+              target = repo_tag === '<none>:<none>' || values[0].include?(ecr_image_key) ? true : false
+
+              if target && !used_images.include?(image.id)
+                @logger.info("  #{image.id}")
+                image.remove(name: repo_tag, force: true)
+              end
             end
           end
         end
