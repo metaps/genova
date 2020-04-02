ba689178ab2940de153ccd9c78fe30b5f73ab631
h-nago <riomuisakuhinata@gmail.com>
2020-04-02 16:43:04 +0900

    remove params

--- lib/genova/slack/bot.rb
+++ lib/genova/slack/bot.rb
@@ -476,7 +476,7 @@ module Genova
         task_definitions = task_definition.task_definition.container_definitions
 
         deployed_commit_id = nil
-        code_manager = Genova::CodeManager::Git.new(params[:account], params[:repository], params)
+        code_manager = Genova::CodeManager::Git.new(params[:account], params[:repository])
 
         task_definitions.each do |task|
           matches = task[:image].match(/(build\-.*$)/)
