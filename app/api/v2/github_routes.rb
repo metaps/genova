eec5f693d49304b6d447f836245b62a3599bf81a
EC2 Default User <n.yamakita@gmail.com>
2021-10-20 17:43:56 +0000

    GitHub Actions

--- app/api/v2/github_routes.rb
+++ app/api/v2/github_routes.rb
@@ -6,17 +6,34 @@ module V2
     resource :github do
       before do
         @payload = request.body.read
-        error! 'Signature is invalid.', 403 unless verify_signature?(@payload)
+        @data = Oj.load(@payload, symbol_keys: true)
       end
 
       # POST /api/v2/github/push
       post :push do
-        id = Genova::Sidekiq::JobStore.create(parse(@payload))
+        error! 'Signature is invalid.', 403 unless verify_webhook_signature?(@payload)
+
+        id = Genova::Sidekiq::JobStore.create(parse_webhook_data(@data))
         Github::DeployWorker.perform_async(id)
 
         { result: 'Deploy request was executed.' }
       rescue Genova::Exceptions::InvalidRequestError => e
         error! e.message, 403
+      end
+
+      # /api/v2/github/actions
+      resource :actions do
+        # POST /api/v2/github/actions/push
+        post :push do
+          error! 'Secret key is invalid.', 403 unless verify_actions_secret_key?
+
+          id = Genova::Sidekiq::JobStore.create(parse_actions_payload(@data))
+          Github::DeployWorker.perform_async(id)
+
+          { result: 'Deploy request was executed.' }
+        rescue Genova::Exceptions::InvalidRequestError => e
+          error! e.message, 403
+        end
       end
     end
   end
--- app/api/v2/helper/github_helper.rb
+++ app/api/v2/helper/github_helper.rb
@@ -3,7 +3,7 @@ module V2
     module GithubHelper
       extend Grape::API::Helpers
 
-      def verify_signature?(payload)
+      def verify_webhook_signature?(payload)
         return false unless request.env['HTTP_X_HUB_SIGNATURE']
 
         digest = OpenSSL::Digest.new('sha1')
@@ -11,27 +11,44 @@ module V2
         payload.present? && Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
       end
 
-      def parse(payload)
-        data = Oj.load(payload, symbol_keys: true)
-        raise Genova::Exceptions::InvalidRequestError, 'Request does not contain "ref" attribute.' if data[:ref].nil?
-
-        matches = data[:ref].match(%r{^refs/([^/]+)/(.+)$})
-
-        # Excludes tag push
-        raise Genova::Exceptions::InvalidRequestError, "#{data[:ref]} is not a valid request." if matches.nil? || matches[1] != 'heads'
-
-        # Exclude commits that don't belong to any branch
-        raise Genova::Exceptions::InvalidRequestError, 'Commit does not belong to any branch' if data[:head_commit].nil?
-
-        account, repository = data[:repository][:full_name].split('/')
+      def parse_webhook_data(data)
 
         {
-          account: account,
-          repository: repository,
-          branch: matches[2],
+          account: data[:repository][:owner][:name],
+          repository: data[:repository][:name],
+          branch: parse_branch(data[:ref]),
           commit_url: data[:head_commit][:url],
           author: data[:head_commit][:author][:username]
         }
+      end
+
+      def verify_actions_secret_key?
+        ENV.fetch('GITHUB_SECRET_KEY') == request.env['HTTP_X_GITHUB_SECRET_KEY']
+      end
+
+      def parse_actions_data(data)
+        {
+          account: data[:account],
+          repository: data[:repository],
+          branch: parse_branch(data[:ref]),
+          commit_url: data[:commit_url],
+          author: data[:author]
+        }
+      end
+
+      private 
+
+      def parse_branch(ref)
+        raise Genova::Exceptions::InvalidRequestError, 'Request does not contain "ref" attribute.' if ref.nil?
+        matches = ref.match(%r{^refs/([^/]+)/(.+)$})
+
+        # Excludes tag push
+        raise Genova::Exceptions::InvalidRequestError, "#{ref} is not a valid request." if matches.nil? || matches[1] != 'heads'
+
+        # Exclude commits that don't belong to any branch
+        # raise Genova::Exceptions::InvalidRequestError, 'Commit does not belong to any branch' if head_commit.nil?
+
+        matches[2]
       end
     end
   end
