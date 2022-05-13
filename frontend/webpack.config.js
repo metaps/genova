9d21398d75a14fd690dcfb148cfe2f2707f5d548
naomichi-y <n.yamakita@gmail.com>
2022-05-13 18:13:34 +0000

    Update frontend packages

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './javascripts/entrypoint.js',
@@ -8,29 +8,18 @@ module.exports = {
     filename: 'javascripts/application.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                plugins: [
-                  require('cssnano')({ preset: 'default' })
-                ]
-              },
-            },
-            'sass-loader'
-          ]
-        })
-      }
-    ]
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }]
   },
   plugins: [
-    new ExtractTextPlugin('stylesheets/application.css')
+    new MiniCssExtractPlugin({
+      filename: 'stylesheets/application.css'
+    })
   ]
 };
