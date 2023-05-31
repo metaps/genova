ab9b1dbb10553eb859e45b7555dd3c384ea78317
naomichi-y <n.yamakita@gmail.com>
2023-05-31 16:03:07 +0000

    Setup vite

--- frontend/javascripts/entrypoint.js
+++
@@ -1,9 +0,0 @@
-import { dom, library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckCircle, faSpinner, faExclamationTriangle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faCheckCircle, faSpinner, faExclamationTriangle, faTimesCircle);
-library.add(faGithub);
-dom.watch();
-
-import '../stylesheets/entrypoint.scss';
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload'
+
+export default defineConfig({
+  plugins: [
+    RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 100 }),
+  ],
+  server: {
+    host: true,
+    port: 3003,
+  },
+})
