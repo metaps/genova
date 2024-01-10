3fe5ca783e08b2a80c6b52da988b17ce64ebeb15
EC2 Default User <n.yamakita@gmail.com>
2024-01-10 09:26:43 +0000

    Support ARM

--- lib/autoloads/genova/version.rb
+++ lib/autoloads/genova/version.rb
@@ -1,8 +1,8 @@
 module Genova
   module Version
-    MAJOR = 4
-    MINOR = 4
-    TINY = 2
+    MAJOR = 5
+    MINOR = 0
+    TINY = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
     LONG_STRING = "genova v#{[MAJOR, MINOR, TINY].join('.')}".freeze
