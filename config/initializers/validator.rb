432983bd41351bab85f6d02d006b41c5ad08f06f
EC2 Default User <n.yamakita@gmail.com>
2024-04-27 10:43:58 +0000

    Fix SSH_PRIVATE_KEY path

--- config/initializers/validator.rb
+++ config/initializers/validator.rb
@@ -1,5 +1,5 @@
 if ENV.fetch('SSH_PRIVATE_KEY', nil).nil?
-  ENV['SSH_PRIVATE_KEY'] = '/app/.ssh/id_rsa'
+  ENV['SSH_PRIVATE_KEY'] = '.ssh/id_rsa'
 
   logger = Logger.new(STDOUT)
   logger.warn("Cannot find the environment variable 'SSH_PRIVATE_KEY' in the .env file, please add 'SSH_PRIVATE_KEY=.ssh/id_rsa'.")
