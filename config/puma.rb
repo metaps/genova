2a492fc425c1dd7f2bfc7705f73572d93c02bb64
EC2 Default User <n.yamakita@gmail.com>
2024-04-28 10:55:29 +0000

    Fix text

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should configured to match
+# Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
