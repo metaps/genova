db79aaecccea3a95266cbbb462bf103f9410d83e
EC2 Default User <n.yamakita@gmail.com>
2024-05-10 10:38:06 +0000

    Fix latest_submodule is always true

--- lib/autoloads/genova/ecs/client.rb
+++ lib/autoloads/genova/ecs/client.rb
@@ -116,7 +116,15 @@ module Genova
         @ecr_client.authenticate
         commit_id = @code_manager.update
 
-        latest_submodule = @code_manager.deploy_config.find_service(@deploy_job.cluster, @deploy_job.service)[:latest_submodule] || true
+        latest_submodule = case type
+                           when :run_task
+                             @code_manager.deploy_config.find_run_task(@deploy_job.cluster, @deploy_job.run_task)[:latest_submodule]
+                           when :service
+                             @code_manager.deploy_config.find_service(@deploy_job.cluster, @deploy_job.service)[:latest_submodule]
+                           when :scheduled_task
+                             @code_manager.deploy_config.find_scheduled_task_rule(@deploy_job.cluster, @deploy_job.scheduled_task_rule)[:latest_submodule]
+                           end
+        latest_submodule = true if latest_submodule.nil?
 
         @code_manager.update_submodule(latest_submodule)
         @deploy_job.update_status_provisioning(commit_id)
