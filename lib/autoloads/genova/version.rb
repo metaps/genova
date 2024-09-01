e410c533c576502e61f516a422d1c38bcc55d839
Naomichi Yamakita <n.yamakita@gmail.com>
2024-09-02 02:01:09 +0900

    Update version.rb

--- lib/autoloads/genova/version.rb
+++ lib/autoloads/genova/version.rb
@@ -1,8 +1,8 @@
 module Genova
   module Version
     MAJOR = 5
-    MINOR = 0
-    TINY = 4
+    MINOR = 1
+    TINY = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
     LONG_STRING = "genova v#{[MAJOR, MINOR, TINY].join('.')}".freeze
