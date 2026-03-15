39c1d66e93e71d7db64b81a995504cef7a3e4bd9
KODA Hiroshi <koda@paymentfor.com>
2026-03-15 18:47:02 +0900

    Secrets Manager/SSM
    Secrets Manager/SSM 対応を追加しました。docker login 用の認証情報は、従来の .env に加えて config/settings.yml の docker.registry.username_secret / password_secret からも取れます。値が arn:aws:secretsmanager: で始まれば Secrets Manager、それ以外は SSM Parameter Store として解決します。secret_fetcher.rb:1 registry.rb:10 settings.yml:11
    
    あわせて、既存の build.secret_args も同じ共通フェッチャを使うように寄せました。これで AWS 由来の秘密値の解決ルールが1か所に揃います。client.rb:96
    
    設定例は以下の通りです。
    
    docker:
      retention_days: 20
      registry:
        url: dhi.io
        username_secret: arn:aws:secretsmanager:ap-northeast-1:123456789012:secret:docker-registry-user
        password_secret: /genova/docker_registry_password
        pull_images:
          - dhi.io/your-org/base:latest

--- spec/lib/autoloads/genova/docker/registry_spec.rb
+++ spec/lib/autoloads/genova/docker/registry_spec.rb
@@ -7,17 +7,20 @@ describe Genova::Docker::Registry do
       url: 'dhi.io',
       username: nil,
       password: nil,
+      username_secret: nil,
+      password_secret: nil,
       pull_images: ['dhi.io/org/app:base']
     )
   end
-  let(:docker_settings) { OpenStruct.new(registry: registry_settings) }
 
   subject(:registry) { described_class.new(logger) }
 
   before do
     allow(Settings).to receive(:dig).with(:docker, :registry).and_return(registry_settings)
-    allow(Settings).to receive(:docker).and_return(docker_settings)
     allow(Genova::Command::Executor).to receive(:call).and_return(0)
+    allow(ENV).to receive(:[]).with('DOCKER_REGISTRY_URL').and_return(nil)
+    allow(ENV).to receive(:[]).with('DOCKER_REGISTRY_USER').and_return(nil)
+    allow(ENV).to receive(:[]).with('DOCKER_REGISTRY_PASSWORD').and_return(nil)
   end
 
   describe '#login_and_pull' do
@@ -39,9 +42,25 @@ describe Genova::Docker::Registry do
       end
     end
 
+    context 'when credentials are provided via Secrets Manager and SSM' do
+      before do
+        registry_settings.username_secret = 'arn:aws:secretsmanager:ap-northeast-1:000000000000:secret:docker-user'
+        registry_settings.password_secret = '/docker/password'
+      end
+
+      it 'resolves credentials and executes docker login' do
+        registry.login_and_pull
+
+        expect(Genova::Command::Executor).to have_received(:call).with(
+          "echo #{Shellwords.escape('PSParameterValue')} | docker login dhi.io -u #{Shellwords.escape('SecretStringType')} --password-stdin",
+          logger,
+          hash_including(filtered_command: 'echo {FILTERED} | docker login dhi.io -u {FILTERED} --password-stdin')
+        )
+      end
+    end
+
     context 'when credentials are missing' do
       before do
-        allow(ENV).to receive(:[]).and_return(nil)
         registry_settings.username = nil
         registry_settings.password = nil
       end
@@ -49,6 +68,18 @@ describe Genova::Docker::Registry do
       it 'skips login and pull' do
         registry.login_and_pull
         expect(Genova::Command::Executor).not_to have_received(:call)
+      end
+    end
+
+    context 'when docker login fails' do
+      before do
+        allow(ENV).to receive(:[]).with('DOCKER_REGISTRY_USER').and_return('user1')
+        allow(ENV).to receive(:[]).with('DOCKER_REGISTRY_PASSWORD').and_return('pass1')
+        allow(Genova::Command::Executor).to receive(:call).and_return(1)
+      end
+
+      it 'raises validation error' do
+        expect { registry.login_and_pull }.to raise_error(Genova::Exceptions::ValidationError, 'Docker registry login failed. [dhi.io]')
       end
     end
   end
